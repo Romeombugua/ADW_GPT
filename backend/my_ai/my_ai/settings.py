83e215de19d53d4dd66a5f139ce8ce6a3583e768
Romeombugua <romeombugua@gmail.com>
2025-05-03 18:12:57 +0300

    changed settings

--- backend/my_ai/my_ai/settings.py
+++ backend/my_ai/my_ai/settings.py
@@ -129,6 +129,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+    
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.1",
+    "92.205.25.96"
 ]
