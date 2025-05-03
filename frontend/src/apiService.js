a6eb12a3657cad5463a5dee8a2df5698220886b4
Romeombugua <romeombugua@gmail.com>
2025-05-03 19:56:17 +0300

    changed api key

--- frontend/src/apiService.js
+++ frontend/src/apiService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8000/api'; // Adjust if your backend runs elsewhere
+const API_BASE_URL = 'https://soback.cbu.net/api'; // Adjust if your backend runs elsewhere
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
